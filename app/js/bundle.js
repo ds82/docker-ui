dfa9a9de0cfdef76a657c0163dc29d180f7539c7
Dennis Sänger <mail@dennis.io>
2015-03-27 14:01:44 +0100

    feat(connection): eastablished connection to docker host

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -10,3 +10,28 @@ function AppCtrl(APP_NAME) {
 
   vm.APP_NAME = APP_NAME;
 }
+
+require('nw.gui').Window.get().showDevTools();
+var fs = require('fs');
+
+var CERT_PATH = process.env.DOCKER_CERT_PATH;
+
+var Docker = require('dockerode');
+var docker3 = new Docker({
+  protocol: 'https',
+  host: 'docker.vm',
+  port: 4243,
+  ca: fs.readFileSync(CERT_PATH + '/ca.pem'),
+  cert: fs.readFileSync(CERT_PATH + '/cert.pem'),
+  key: fs.readFileSync(CERT_PATH + '/key.pem')
+});
+
+console.log(docker3);
+
+docker3.listContainers({all: true}, function(err, containers) {
+  console.log(err);
+  console.log('ALL: ' + containers);
+  containers.forEach(function(container) {
+    console.dir(container);
+  });
+});
